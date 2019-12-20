b0ded732477e4c9c219085e724bb090d4318ff27
john_money <john.money@oracle.com>
2019-12-20 03:11:34 -0500

    alter content_id column

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE asset_event (
   event_id VARCHAR(36) PRIMARY KEY,
   event_type VARCHAR(32) NOT NULL,
-  content_id VARCHAR(36) NOT NULL,
+  content_id VARCHAR(50) NOT NULL,
   user_id VARCHAR(255) NOT NULL,
   event_time timestamp DEFAULT current_timestamp
 );
