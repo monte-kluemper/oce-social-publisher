3da325bdf556986b1c79829210e5248182e6d522
Monte Kluemper <monte.kluemper@oracle.com>
2021-04-27 12:05:56 +0200

    mk1

--- indexJohn.js
+++ indexJohn.js
@@ -24,21 +24,32 @@ app.use(helmet())
 const webhookChannel = async (req, res) => {
 	try {
 		// validate the payload schema
-    console.log("WebhookChannel:"+req.body);
-		if (false) {
+		if (!req.body.event.id || !req.body.event.name || !req.body.event.initiatedBy || !req.body.entity.id || !req.body.entity.items[0].length) {
 			res.status(400).send({ status: 400, error: 'Request not properly formed' })
-		} else {
+		}
+		else {
+			// iterate items to extract details
+			var assets = [];
+			req.body.entity.items[0].forEach(function(item){
+				assets.push({
+					id: item.id,
+					type: item.type,
+					name: item.name
+				});
+			});
 
 			try {
-        console.log("WebhookChannel:try:"+req.body);
+				const client = await pool.connect()
+				const result = await client.query('INSERT INTO channel_event (event_id, event_type, channel_id, user_id, assets) VALUES ($1, $2, $3, $4, $5)', [req.body.event.id, req.body.event.name, req.body.entity.id, req.body.event.initiatedBy, {assets}]);
+				console.log('* Inserted new webhookChannel event')
 				res.status(201).json({status: 201})
 			} catch (err) {
-				console.error('ERROR: WebhookChannel 400 error:\n' + err);
+				console.error('** webhookChannel insert error:\n' + err);
 				res.status(400).send({ status: 400, error: 'Could not process request' })
 			}
 		}
 	} catch (err) {
-    console.error('ERROR: WebhookChannel 500 error:\n' + err);
+		console.error('* webhookChannel error:\n' + err);
 		res.status(500).send({ status: 500, error: 'Existential server error' })
 	}
 }
@@ -47,25 +58,25 @@ const webhookChannel = async (req, res) => {
 const webhookRepository = async (req, res) => {
 	try {
 		// validate the payload schema
-    console.log("WebhookRepository:"+req.body);
-		if (false) {
+		if (!req.body.event.id || !req.body.event.name || !req.body.event.initiatedBy || !req.body.entity.repositoryId) {
 			res.status(400).send({ status: 400, error: 'Request not properly formed' })
-		} else {
-
+		}
+		else {
 			// extract channel
 			var channels = !req.body.event.channelIds ? [] : req.body.event.channelIds;
-      console.log("WebhookRepository:channels:"+channels);
 
-      try {
-        console.log("WebhookRepository:try:"+req.body);
+			try {
+				const client = await pool.connect()
+				const result = await client.query('INSERT INTO repository_event (event_id, event_type, repository_id, user_id, channel_id, asset) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.event.id, req.body.event.name, req.body.entity.repositoryId, req.body.event.initiatedBy, {channels}, req.body.entity]);
+				console.log('* Inserted new webhookRepository event')
 				res.status(201).json({status: 201})
 			} catch (err) {
-				console.error('ERROR: WebhookRepository 400 error:\n' + err);
+				console.error('** webhookRepository insert error:\n' + err);
 				res.status(400).send({ status: 400, error: 'Could not process request' })
 			}
 		}
 	} catch (err) {
-		console.error('* WebhookRepository error:\n' + err);
+		console.error('* webhookRepository error:\n' + err);
 		res.status(500).send({ status: 500, error: 'Existential server error' })
 	}
 }
@@ -77,3 +88,4 @@ app
 	   res.status(404).send('Resource not found');
   })
   .listen(PORT, () => console.log(`OCE Webook Node.js listening on ${ PORT }`))
+  
